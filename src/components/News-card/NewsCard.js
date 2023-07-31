ea89493c9a66950cbb96a910821d75de67528392
abhirawat623 <abhirawat623@gmail.com>
2023-08-01 01:15:24 +0530

    fetching done

--- src/components/News-card/NewsCard.js
+++ src/components/News-card/NewsCard.js
@@ -1,31 +1,43 @@
 import { useState } from "react";
-const NewsCard = ({data})=>{
-console.log(data)
+const NewsCard = ({props})=>{
+console.log(props)
+ 
+
+ 
 
 
 
-  const news = data.articles[7];
+    
+  
 
     return(
      
-        <div>
-           <div className="card">
-     <div className="card-image-container">
-          <img className="card-image" src={`${news.urlToImage}`} alt="image"/>
-     </div>
-     <div className="news-details">
-          <div className="news-title">{news.title}</div>
-          <div className="news-description">
-               <p className="card-des">{news.content}</p>
-               <p className="card-price">
-                 
-               	  <span className="price-strike-through">\</span>
-                  <span className="autor">{news.author}</span>
-               </p>
-          </div>
-         
-     </div>
-</div>
+        <div className="card-container">
+
+
+          { props.map((value)=>{
+
+               return(
+            <div className="card">
+            <div className="card-image-container">
+            <a href={value.url} className="card-know-more">
+            <img className="card-image" src={value.urlToImage} alt="image"/></a>
+            </div>
+              <div className="news-details">
+                 <div className="news-title">{value.content}
+                 </div>
+                 <div className="news-description">
+                      <p className="card-des">{value.description}</p>
+                        <p className="card-published">{value.publishedAt}</p>
+                          
+                            <span className="author">{value.author}</span>
+                        
+                 </div>
+                
+               </div>
+            </div>)
+          })}
+           
         </div>
     )
 }
--- src/components/News-container/NewsContainer.js
+++ src/components/News-container/NewsContainer.js
@@ -3,36 +3,37 @@ import NewsCard from "../News-card/NewsCard";
 import axios  from "axios";
  const NewsContainer=()=>{
 
-const [items,setItems]= useState([]);
+const [news,setNews]= useState([]);
 
 
     useEffect(()=>{
     async function fetchData(){
         try{
-             const response = await axios.get('https://newsapi.org/v2/everything?q=keyword&apiKey=8a4f493837be4924b72307cb5ca09e67');
-             const data = [response.data];
-         
-             setItems(data);
+             const response = await axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=8a4f493837be4924b72307cb5ca09e67');
+             const data = response.data.articles
+             const transformedData = data.map( (item,index)=>{
+               return{
+                ...item,
+                id:index
+               }
+             })
+             setNews(transformedData);
         }
         catch(err){
              console.log("error",err);
-             alert("Some error occured")
+             alert("Some error occured in the api link")
         }};
          fetchData()
        
    },[])
 
+  
+    
     return(
        
-        <div className="card-container">
-          
-          {items.map(item=>{
-           
-
-  return (<NewsCard data={item}/>)})
-}
-
-        </div>
+     <>
+     <NewsCard key={news.id} props={news}/>
+     </>
     )
 }
 export default NewsContainer;
